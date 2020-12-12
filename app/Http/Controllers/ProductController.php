252978fccb654d184832a080632ba92c8754e0c4
Tronghien18 <votronghien18@gmail.com>
2020-12-12 15:50:13 +0700

    Them product detail

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -40,7 +40,7 @@ class ProductController extends Controller
 	}
 	public function getProductDetail($id){
 		$product = Product::find($id);
-
+		///show 
 		if($product){
 			return view('ban-banh.product-detail', array(
 				'product' => $product,
