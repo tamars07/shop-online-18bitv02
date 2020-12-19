f54879dc6c923b808c2cfda399afad42489d725b
Trần Tâm <tamars07@yahoo.com>
2020-12-19 15:09:30 +0700

    migration files

--- app/Customer.php
+++ app/Customer.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     //
-    protected $table = 'customer';
+    protected $table = 'customers';
 }
--- database/migrations/2020_12_19_075349_create_bills_table.php
+++ database/migrations/2020_12_19_075349_create_bills_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBillsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bills', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('id_customer');
+            $table->date('date_order');
+            $table->double('total')->defaut(0);
+            $table->string('payment',200)->nullable();
+            $table->text('note')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bills');
     }
 }
